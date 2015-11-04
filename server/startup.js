1ab1f1b2c7011d891e1faa2deb1e3ae892fdc405
Kris <kris@urbanscouter.com>
2015-11-04 02:57:37 -0600

    Updated DB field names and added job details

--- server/startup.js
+++ server/startup.js
@@ -2,16 +2,16 @@ Meteor.startup(function () {
 	if (Jobs.find().count() === 0) {
 		var jobs = [
 	      {
-	        'name': 'Oseberg',
-	        'description': 'Meteor/Angular designer.'
+	        'company': 'Oseberg',
+	        'position': 'Meteor/Angular designer.'
 	      },
 	      {
-	        'name': 'Nodecraft',
-	        'description': 'Dev Ops Duder'
+	        'company': 'Nodecraft',
+	        'position': 'Dev Ops Duder'
 	      },
 	      {
-	        'name': 'Tailwind',
-	        'description': 'PHP job here'
+	        'company': 'Tailwind',
+	        'position': 'PHP job here'
 	      }
 	    ];
 
