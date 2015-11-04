f9b352e87dac33a7ddd56b9944bc7be5b3114144
Kris <kris@urbanscouter.com>
2015-11-03 23:03:58 -0600

    Added job details

--- client/app.ng.js
+++ client/app.ng.js
@@ -1,9 +1,33 @@
- angular.module('theBoard', ['angular-meteor']);
+ angular.module('theBoard', ['angular-meteor', 'ui.router']);
+
+   angular.module('theBoard').config(function($urlRouterProvider, $stateProvider, $locationProvider){
  
-  angular.module('theBoard').controller('jobListCtrl', function ($scope, $meteor) {
+      $locationProvider.html5Mode(true);
+ 
+      $stateProvider
+        .state('jobs', {
+          url: '/jobs',
+          templateUrl: 'client/jobs-list.ng.html',
+          controller: 'jobsListCtrl'
+        })
+        .state('partyDetails', {
+          url: '/jobs/:partyId',
+          templateUrl: 'client/job-details.ng.html',
+          controller: 'jobDetailsCtrl'
+        });
+ 
+      $urlRouterProvider.otherwise("/jobs");
+    });
+ 
+  angular.module('theBoard').controller('jobsListCtrl', function ($scope, $meteor) {
     $scope.jobs = $meteor.collection(Jobs);
 
     $scope.remove = function(job){
       $scope.jobs.remove(job);
     };
+
+  });
+
+  angular.module("theBoard").controller("jobDetailsCtrl", function($scope, $stateParams) {
+    $scope.partyId = $stateParams.partyId;
   });
