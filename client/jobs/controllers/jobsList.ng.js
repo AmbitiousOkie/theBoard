baf5071ad37300b17f6a58d43397d66e90c181d9
Kris <kris@urbanscouter.com>
2015-11-06 03:18:54 -0600

    Added a delete listing button for current users

--- client/jobs/controllers/jobsList.ng.js
+++ client/jobs/controllers/jobsList.ng.js
@@ -2,4 +2,16 @@
     $scope.jobs = $meteor.collection(Jobs).subscribe('jobs');
 
     var loggedIn = true;
+
+    $scope.remove = function(job){
+    	if ($scope.currentUser == $scope.job.owner) {
+      		$scope.jobs.splice($scope.jobs.indexOf(job), 1);
+      	}
+    };
+
+    $scope.debug = function(job) {
+    	console.log('currentUser is ' + $scope.currentUser + '\n');
+    	console.log('job owner is ' + $job.owner + '\n');
+    }
+
   });
