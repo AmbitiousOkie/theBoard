7d19497c98c253d4bccbda82ba4090a0bd457fe0
Kris <kris@urbanscouter.com>
2015-11-06 01:40:51 -0600

    Added a public toggle / removed autopublish

--- client/jobs/controllers/jobsList.ng.js
+++ client/jobs/controllers/jobsList.ng.js
@@ -1,5 +1,5 @@
   angular.module('theBoard').controller('JobsListCtrl', function ($scope, $meteor) {
-    $scope.jobs = $meteor.collection(Jobs);
+    $scope.jobs = $meteor.collection(Jobs).subscribe('jobs');
 
     $scope.remove = function(job){
       $scope.jobs.splice($scope.jobs.indexOf(job), 1);
---
+++ server/jobs.js
@@ -0,0 +1,13 @@
+Meteor.publish("jobs", function () {
+  return Jobs.find({
+    $or:[
+      {$and:[
+        {"public": true},
+        {"public": {$exists: true}}
+      ]},
+      {$and:[
+        {owner: this.userId},
+        {owner: {$exists: true}}
+      ]}
+    ]});
+});
